1faf519c3d96a58ecdd556106083d3151228a0f7
YilongJu <juyilong@gmail.com>
2022-07-03 10:38:07 -0700

    Added auto_mask plotting for time series

--- src/utils/ECG_data_plotting.py
+++ src/utils/ECG_data_plotting.py
@@ -4,4 +4,63 @@ import torch
 import math
 import warnings
 import numpy as np
+
 from PIL import Image, ImageDraw, ImageFont, ImageColor
+import io
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import seaborn as sns
+
+def Save_time_series_as_img_torch_array(signal, label=None):
+    plt.rcParams["figure.figsize"] = [7.50, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+    plt.rcParams["backend"] = "agg"
+    sns.set(style="ticks", font_scale=1.33)
+    plt.figure()
+    if label is not None:
+        if isinstance(label, str):
+            legend_label = label
+        else:
+            if isinstance(label, torch.Tensor):
+                label = label.item()
+            legend_label = "JET" if label == 1 else "Sinus"
+    else:
+        legend_label = None
+
+    if isinstance(label, str):
+        color = cm.tab10(0)
+    elif label is None:
+        color = cm.tab10(0)
+    else:
+        color = cm.tab10(label + 1)
+
+    plt.plot(signal, color=color, label=legend_label)
+    if legend_label is not None:
+        plt.legend(loc="upper right")
+    img_buf = io.BytesIO()
+    plt.savefig(img_buf, format='png')
+    im = Image.open(img_buf)
+    im_array_torch = torch.tensor(np.array(im))
+    return im_array_torch.transpose(2, 0).transpose(2, 1).unsqueeze(0)[:, :3, ...]
+
+
+def Convert_batch_of_time_series_to_batch_of_img_torch_array(signals, labels=None, add_ind_to_legend=False):
+    if labels is None:
+        labels = [None] * len(signals)
+
+    if isinstance(labels, str):
+        labels = [labels] * len(signals)
+
+    img_array_batch_list = []
+    for i, (signal, label) in enumerate(zip(signals, labels)):
+        # print(i, signal.shape)
+        signal_reshaped = signal.squeeze(0)
+        if isinstance(label, str):
+            label_processed = f"{label}_{i}" if add_ind_to_legend else label
+        else:
+            label_processed = label
+        img_array_torch_reshaped = Save_time_series_as_img_torch_array(signal_reshaped, label_processed)
+        # print(img_array_torch_reshaped.shape)
+        img_array_batch_list.append(img_array_torch_reshaped)
+
+    return torch.cat(img_array_batch_list, dim=0)
--- src/utils/auto_mask.py
+++ src/utils/auto_mask.py
@@ -6,11 +6,8 @@ from torchvision.utils import save_image
 import pytorch_lightning as pl
 import torch
 from pytorch_lightning.callbacks import Callback
-import matplotlib.pyplot as plt
 
-
-import io
-from PIL import Image
+from src.utils.ECG_data_plotting import Convert_batch_of_time_series_to_batch_of_img_torch_array
 
 class AutoMASK(Callback):
     def __init__(
@@ -107,39 +104,40 @@ class AutoMASK(Callback):
             module.eval()
             with torch.no_grad():
                 for n, (x, y) in enumerate(trainer.val_dataloaders[0]):
-                    x = x.to(device, non_blocking=True)[:8]
-                    y = y.to(device, non_blocking=True)[:8].int()
+                    x = x.to(device, non_blocking=True)[:8] # B * C * W * H / B * 1 * W
+                    y = y.to(device, non_blocking=True)[:8].int() # B
                     feats = module.mask_encoder(x)
                     soft_masks = module.mask_head(feats)
                     a = soft_masks.argmax(dim=1).cpu()
                     hard_masks = torch.zeros(soft_masks.shape).scatter(1, a.unsqueeze(1), 1.0)
-                    if len(mask.shape) == 4:
-                        save_tensor = [x.cpu()]
-                    elif len(mask.shape) == 3:
+                    if len(x.shape) == 4:
+                        input_img = x.cpu()
+                    elif len(x.shape) == 3: # Time series
                         # TODO: Save ECG plots to buffer and convert to data matrix for plotting
-                        plt.rcParams["figure.figsize"] = [7.50, 3.50]
-                        plt.rcParams["figure.autolayout"] = True
-                        plt.figure()
-                        plt.plot([1, 2])
+                        """
+                        References: 
+                        - https://www.tutorialspoint.com/how-to-convert-matplotlib-figure-to-pil-image-object
+                        - https://stackoverflow.com/questions/384759/how-to-convert-a-pil-image-into-a-numpy-array
+                        """
+                        input_img = Convert_batch_of_time_series_to_batch_of_img_torch_array(x.cpu())
+                    print(f"input_img.shape = {input_img.shape}")
+                    save_tensor = [input_img]
 
-                        img_buf = io.BytesIO()
-                        plt.savefig(img_buf, format='png')
-
-                        save_tensor = []
-                    # print(f"x.shape = {x.shape}")
                     for mask in torch.chunk(hard_masks, self.args.N, dim=1):
                         # print(f"mask.shape = {mask.shape}")
 
                         if len(mask.shape) == 4:
-                            save_tensor.extend([mask.repeat(1,3,1,1), x.cpu() * (1 - mask)])
+                            save_tensor.extend([mask.repeat(1,3,1,1), input_img * (1 - mask)])
                         elif len(mask.shape) == 3:
-                            save_tensor.extend([mask.repeat(1, 1, 1), x.cpu() * (1 - mask)])
+                            # 'mask' is a time seris
+                            mask_img = Convert_batch_of_time_series_to_batch_of_img_torch_array(mask.repeat(1, 1, 1))
+                            masked_input_img = Convert_batch_of_time_series_to_batch_of_img_torch_array(x.cpu() * (1 - mask))
+                            save_tensor.extend([mask_img, masked_input_img])
                         else:
                             raise NotImplementedError("Unknown mask shape.")
 
                         # print(f"save_tensor[-2].shape = {save_tensor[-2].shape}")
                         # print(f"save_tensor[-1].shape = {save_tensor[-1].shape}")
-
 
                     path = os.path.join(self.path, self.umap_placeholder.format(trainer.current_epoch, n))
                     save_image(torch.cat(save_tensor), path)
