6534d02c5ec4b777260d2951192bb64fa1289b59
YilongJu <juyilong@gmail.com>
2022-10-17 02:45:15 -0400

    Update ECG_data_loading.py

--- src/utils/ECG_data_loading.py
+++ src/utils/ECG_data_loading.py
@@ -131,6 +131,24 @@ def Lower(word):
     """ Convert word to lower case """
     return word.lower()
 
+""" https://stackoverflow.com/questions/6518811/interpolate-nan-values-in-a-numpy-array """
+def nan_helper(y):
+    """Helper to handle indices and logical indices of NaNs.
+
+    Input:
+        - y, 1d numpy array with possible NaNs
+    Output:
+        - nans, logical indices of NaNs
+        - index, a function, with signature indices= index(logical_indices),
+          to convert logical indices of NaNs to 'equivalent' indices
+    Example:
+        >>> # linear interpolation of NaNs
+        >>> nans, x= nan_helper(y)
+        >>> y[nans]= np.interp(x(nans), x(~nans), y[~nans])
+    """
+
+    return np.isnan(y), lambda z: z.nonzero()[0]
+
 class ECG_classification_dataset_with_peak_features(Dataset):
     def __init__(self, feature_df_all_selected_p_ind_with_ecg, ecg_resampling_length_target=300,
                  peak_loc_name="p_ind_resampled", label_name="label", short_identifier_list=None,
