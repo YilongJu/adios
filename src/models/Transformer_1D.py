0a44f954dde77306f106914b48995a471b801070
YilongJu <juyilong@gmail.com>
2022-11-08 00:38:02 -0500

    updated positional encoding function

--- src/models/Transformer_1D.py
+++ src/models/Transformer_1D.py
@@ -18,7 +18,7 @@ class PositionalEncoding(nn.Module):
 
     def __init__(self, d_model, dropout=0.1, max_len=5000):
         super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=0.1)
+        self.dropout = nn.Dropout(p=dropout)
 
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
@@ -26,13 +26,11 @@ class PositionalEncoding(nn.Module):
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
+        self.register_parameter('pe', nn.Parameter(pe, requires_grad=False))
 
     def forward(self, x):
-        # x = x + self.pe[:x.size(1), :].squeeze(1)
         x = x + self.pe[:x.size(0), :]
-        # return self.dropout(x)
-        return x
+        return self.dropout(x)
 
 
 class SelfAttentionPooling(nn.Module):
