a12ca519d8610e19bfb4aa55aee6017a659678b4
YilongJu <juyilong@gmail.com>
2022-11-08 10:58:51 -0500

    us

--- bash_files/ECG_debug/run_sweep.sh
+++ bash_files/ECG_debug/run_sweep.sh
@@ -142,3 +142,5 @@ python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5,6,7,8 --prefix b
 python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5 --prefix a --cuda --command "wandb agent yilongju/adios_ecg-supervised-transformer_1D-ECG_normalized-20221107_v22/7u3mvlv4"
 python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5 --prefix b --cuda --command "wandb agent yilongju/adios_ecg-supervised-transformer_1D-ECG_normalized-20221107_v22/7u3mvlv4"
 
+python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5 --prefix a --stop
+python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5 --prefix b --stop
