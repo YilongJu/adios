9205e910047430b28c7dd7276dea5795699fa4e1
YilongJu <juyilong@gmail.com>
2022-12-09 09:52:26 -0500

    Added LIME

--- bash_files/ECG_debug/run_sweep.sh
+++ bash_files/ECG_debug/run_sweep.sh
@@ -196,6 +196,12 @@ python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5,6,7,8 --prefix b
 python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5 --prefix a --cuda --command "wandb agent yilongju/adios_ecg-supervised-clocsCNN_1D-ECG_normalized-20221202_v30/fjtvwtqe"
 python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5 --prefix b --cuda --command "wandb agent yilongju/adios_ecg-supervised-clocsCNN_1D-ECG_normalized-20221202_v30/fjtvwtqe"
 
+# 20221202 ECG (original, without CVP) + ResNet18 1D + data augmentation
+python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5,6,7,8 --prefix a --cuda --command "wandb agent yilongju/adios_ecg-supervised-ResNet18_1D-ECG_normalized-20221204_v31/4lzvtufi"
+python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5,6,7,8 --prefix b --cuda --command "wandb agent yilongju/adios_ecg-supervised-ResNet18_1D-ECG_normalized-20221204_v31/4lzvtufi"
+
+python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5 --prefix a --cuda --command "wandb agent yilongju/adios_ecg-supervised-ResNet18_1D-ECG_normalized-20221204_v31/4lzvtufi"
+python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5 --prefix b --cuda --command "wandb agent yilongju/adios_ecg-supervised-ResNet18_1D-ECG_normalized-20221204_v31/4lzvtufi"
 
 
 
