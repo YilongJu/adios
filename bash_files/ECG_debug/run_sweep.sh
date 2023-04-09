67cd2ec4bd0107ad64894ccd64226e0b888b3fba
YilongJu <juyilong@gmail.com>
2023-04-09 00:33:54 -0400

    us

--- bash_files/ECG_debug/run_sweep.sh
+++ bash_files/ECG_debug/run_sweep.sh
@@ -275,6 +275,11 @@ python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5,6,7,8 --prefix a
 python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5,6,7,8 --prefix b --cuda --command "wandb agent yilongju/adios_ecg-supervised-clocsCNN_1D-ECG_normalized-20230220_v30-deep_ensemble/1a7od81n"
 python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5,6,7,8 --prefix c --cuda --command "wandb agent yilongju/adios_ecg-supervised-clocsCNN_1D-ECG_normalized-20230220_v30-deep_ensemble/1a7od81n"
 
+# 20230409 ECG + CVP + ClocsCNN
+python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5,6,7,8 --prefix a --cuda --command " wandb agent yilongju/adios_ecg-supervised-clocsCNN_1D-ECG_normalized-20230220_v30-deep_ensemble/z3ghnbll"
+python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5,6,7,8 --prefix b --cuda --command " wandb agent yilongju/adios_ecg-supervised-clocsCNN_1D-ECG_normalized-20230220_v30-deep_ensemble/z3ghnbll"
+python Send_commands_to_screen_session.py --gpu_ids 0,1,2,3,4,5,6,7,8 --prefix c --cuda --command " wandb agent yilongju/adios_ecg-supervised-clocsCNN_1D-ECG_normalized-20230220_v30-deep_ensemble/z3ghnbll"
+
 
 
 
